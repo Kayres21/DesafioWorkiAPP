9fdf717120efefe21f07c1de1efb587b04678789
Erick Viera <erick.viera@sansano.usm.cl>
2021-02-15 18:30:15 -0300

    creación de pdf lista

---
+++ python parte 2/pdf.py
@@ -0,0 +1,43 @@
+from fpdf import FPDF 
+import csv
+
+
+def make_pdf():
+
+        with open('result.csv', newline= '') as f:
+                reader = csv.reader(f)
+                reader
+                        
+                pdf = FPDF()
+
+                pdf.add_page()
+
+                page_width = pdf.w - 2 * pdf.l_margin
+
+                pdf.set_font('Times','B',20.0) 
+                pdf.cell(page_width, 0.0, 'Usuarios Data', align='C')
+                pdf.ln(10)
+
+                pdf.set_font('Courier', '', 8)
+
+                col_width = page_width/6
+
+                pdf.ln(1)
+
+                th = pdf.font_size
+
+                for row in reader:
+                        pdf.cell(col_width*1.5, th, str(row[0].encode('mbcs').decode('utf8')), border=1)
+                        pdf.cell(col_width, th, row[2].encode('mbcs').decode('utf8'), border=1)
+                        pdf.cell(col_width, th, row[3].encode('mbcs').decode('utf8'), border=1)
+                        pdf.cell(col_width*1.4, th, row[4].encode('mbcs').decode('utf8'), border=1)
+                        pdf.cell(col_width*0.7, th, row[5].encode('mbcs').decode('utf8'), border=1)
+                        pdf.ln(th)
+
+                pdf.ln(10)
+
+                pdf.set_font('Times','',10.0) 
+                
+
+                pdf.output('resultados.pdf', 'F')
+
---
+++ python parte 2/script.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+import pandas as pd
+import pdf
+
+
+load_dotenv()
+
+
+
+def connection():
+    DATABASE = os.getenv('DATABASE')
+    DATABASE_USER = os.getenv('DATABASE_USER')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
+
+    connection = psycopg2.connect(database= DATABASE, user= DATABASE_USER, password= DATABASE_PASSWORD, host= DATABASE_HOST, port= DATABASE_PORT)
+    cursor = connection.cursor()
+
+    return cursor 
+
+def get_data(cursor, query):
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return result
+
+
+def grup(data):
+    datos  = pd.DataFrame(data, columns=["Empresa", "Id_empresa", "Nombre_usuario", "Apellido_usuario", "Ultimo_ingreso", "Rut"], dtype = str ) 
+    datos_agrupados =datos.groupby('Empresa')
+    datos.to_csv('./result.csv', index=False)
+    
+
+def main():
+    cursor = connection()
+
+    query = """SELECT 
+    companies.name AS company_name,
+    companies.id AS company_id,
+    users.name AS name_user,
+    users.last_name AS last_name_user,
+    users.audit -> '5c7dd876-39e4-425b-86bd-6a43adcf95d5' ->>'last_login' AS last_login,
+    users.properties -> '5c7dd876-39e4-425b-86bd-6a43adcf95d5' ->>'rut' AS rut
+    FROM companies INNER JOIN users ON companies.id = users.company_id ORDER BY company_name"""
+    resultado = get_data(cursor, query)
+    grup(resultado)
+    pdf.make_pdf()
+    
+
+
+
+
+
+main()
+    
+
+  
